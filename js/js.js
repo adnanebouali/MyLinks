74497c6900f076a9e1d47619e9bfdf043cc10700
adnanebouali <adnanbouali1@gmail.com>
2023-05-04 11:04:30 +0100

    Quote generator

--- js/js.js
+++ js/js.js
@@ -61,3 +61,46 @@ el.addEventListener("mousedown", function () {
 el.addEventListener("mouseup", function () {
   el.style.transform = "perspective(500px)  rotateX(0) rotateY(0)";
 });
+
+const quoteElement = document.querySelector("#quote");
+
+function getQuote() {
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      quoteElement.textContent = `"${data.content}" - ${data.author}`;
+      quoteElement.style.backgroundColor = getRandomColor();
+    })
+    .catch((error) => {
+      console.error(error);
+      quoteElement.textContent =
+        "Self-belief and hard work will always earn you success.";
+    });
+}
+
+function getRandomColor() {
+  const colors = [
+    "rgb(220, 226, 255)", // Red
+    "#E3F2C1", // Pink
+    "#917FB3", // Purple
+    "#BFCCB5", // Deep Purple
+    "#DDFFBB", // Indigo
+    "#F6F1E9", // Blue
+    "#FFB4B4", // Light Blue
+    "#F5C6EC", // Cyan
+    "#D5B4B4", // Teal
+    "#AEC2B6", // Green
+    "#FFACAC", // Light Green
+    "#EDE9D5", // Amber
+    "#F7C8E0", // Orange
+    "#BEF0CB", // Deep Orange
+    "#D9ACF5", // Brown
+    "#B7B78A", // Grey
+    "#E3ACF9", // Blue Grey
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+window.onload = function () {
+  getQuote();
+};
